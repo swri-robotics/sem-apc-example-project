58e3430c6cf6a4b91d8442b7966880acf44cd412
Fillipko <fillip.cannard@gmail.com>
2026-03-23 15:39:02 -0500

    Set use_sim_time parameter in launch file

--- project/shell_simulation/launch/shell_simulation.launch.py
+++ project/shell_simulation/launch/shell_simulation.launch.py
@@ -1,8 +1,13 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 
 # Example ROS launch file
 def generate_launch_description():
+    
+    # This sets use_sim_time=True for all subsequent nodes in this launch file, 
+    # which is required for correct time synchronization with the CARLA server 
+    # when using ROS time.
+    SetParameter(name='use_sim_time', value=True),
     
     example_control = Node(
         package='shell_simulation',
